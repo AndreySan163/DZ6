e6105bfb201778282bf5bf1e42890321e848eb1a
Andrei <nestorivanov163@gmail.com>
2023-10-28 07:36:58 +0400

    DZ66

--- src/DZ6/Main.java
+++ src/DZ6/Main.java
@@ -1,50 +1,33 @@
+package DZ6;
+
 public class Main {
     public static void main(String[] args) {
+        Phone phone = new Phone();
+        phone.setNumber("+7 999 000 000");
+        phone.setModel("Motorola");
+        phone.setWeight(135.9);
+
+        phone.receiveCall("Иван");
+        phone.info();
+        System.out.println();
+
+        Phone phone1 = new Phone();
+        phone.setNumber("+7 999 111 111");
+        phone.setModel("Nokia");
+        phone.setWeight(150.8);
+
+        phone.receiveCall("Николай");
+        phone.info();
+        System.out.println();
+
+        Phone phone2 = new Phone();
+        phone.setNumber("+7 999 222 222");
+        phone.setModel("Sumsung");
+        phone.setWeight(177.3);
+
+        phone.receiveCall("Пётр");
+        phone.info();
 
     }
 }
 
-
-/*class Phone:
-    def __init__(self, number, model, weight):
-        self.number = number
-        self.model = model
-        self.weight = weight
-
-    def receiveCall(self, name):
-        print(f"Звонит {name}")
-
-    def getNumber(self):
-        return self.number
-
-# Создание трех экземпляров класса Phone с различными конструкторами
-phone1 = Phone("123456789", "iPhone X", 150)
-phone2 = Phone("987654321", "Samsung Galaxy S20", 170)
-phone3 = Phone("111222333", "Google Pixel 4", 160)
-
-# Вывод значений переменных экземпляров на консоль
-print("Телефон 1:")
-print("Номер:", phone1.number)
-print("Модель:", phone1.model)
-print("Вес:", phone1.weight)
-
-print("Телефон 2:")
-print("Номер:", phone2.number)
-print("Модель:", phone2.model)
-print("Вес:", phone2.weight)
-
-print("Телефон 3:")
-print("Номер:", phone3.number)
-print("Модель:", phone3.model)
-print("Вес:", phone3.weight)
-
-# Вызов метода receiveCall для всех экземпляров класса
-phone1.receiveCall("Анна")
-phone2.receiveCall("Петр")
-phone3.receiveCall("Мария")
-
-# Вызов метода getNumber для всех экземпляров класса и вывод значения на консоль
-print("Номер телефона Phone 1:", phone1.getNumber())
-print("Номер телефона Phone 2:", phone2.getNumber())
-print("Номер телефона Phone 3:", phone3.getNumber())
-*/
--- src/DZ6/Phone.java
+++ src/DZ6/Phone.java
@@ -1,2 +1,55 @@
-package PACKAGE_NAME;public class Phone {
+package DZ6;
+
+public class Phone {
+
+    private String number;
+    private String model;
+    private double weight;
+
+    public Phone() {
+        System.out.println("Телефон создан");
+    }
+
+    public Phone(String number, String model) {
+        this.number = number;
+        this.model = model;
+    }
+
+    public Phone(String number, String model, double weight) {
+        this.number = number;
+        this.model = model;
+        this.weight = weight;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public void receiveCall(String name){
+        System.out.println("Звонит Иван");
+    }
+
+    public void  info(){
+        System.out.printf("Phone: number = %s\nmodel = %s\nweight = %s\n", this.getNumber(), this.getModel(), this.getWeight());
+    }
 }
